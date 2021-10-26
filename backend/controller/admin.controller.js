7a9f2d4d8ff73e69c584799708d12b412d388958
sarocha-changEdit profilesarocha-chang <sarocha_chang@cmu.ac.th>
2021-10-26 17:33:14 +0700

    function for admin finish 1/3 and function for all finish

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -1,34 +1,114 @@
 const {items, category, customers} = require("../model");
 module.exports = {
 	addItem: async (req, res, next) => {
+		try {
+			const item = await items.create(req.body);
+			if (item) {
+				res.status(200).json({
+					message: "เพิ่มสินค้าสำเร็จ!",
+					item,
+				});
+			} else {
+				res.status(500).json({
+					message: "ไม่สามารถเพิ่มสินค้าได้!",
+				});
+			}
+		} catch (error) {
+			if (error.errors) {
+				error.errors = error.errors.map((err) => [err.message, err.path, err.instance[err.path]]);
+				error.errors = error.errors.map((err) => {
+					return {
+						message: err[0],
+						path: err[1],
+						value: err[2],
+					};
+				});
+				res.status(500).json(error.errors);
+			} else {
+				res.status(500).json(error);
+			}
+		}
+		next();
 	},
 
 	deleteItem: async (req, res, next) => {
+		try {
+			const item = await items.findByPk(req.params.id);
+			if (item) {
+				await item.destroy({
+					where: {
+						id: item.id,
+					},
+				});
+				res.status(200).json({
+					message: "ลบสินค้าสำเร็จ!",
+				});
+			} else {
+				res.status(404).json({
+					message: "ไม่พบสินค้าในระบบ!",
+				});
+			}
+		} catch (error) {
+			if (error.errors) {
+				error.errors = error.errors.map((err) => [err.message, err.path, err.instance[err.path]]);
+				error.errors = error.errors.map((err) => {
+					return {
+						message: err[0],
+						path: err[1],
+						value: err[2],
+					};
+				});
+				res.status(500).json(error.errors);
+			} else {
+				res.status(500).json(error);
+			}
+		}
+		next();
 	},
 
 	updateItem: async (req, res, next) => {
-			},
-
-	showCategory: async (req, res, next) => {
-
+		try {
+			const item = await items.findByPk(req.params.id);
+			if (item) {
+				await item.update(req.body);
+				res.status(200).json({
+					message: "อัพเดทข้อมูลสินค้าเรียบร้อย!",
+					item,
+				});
+			} else {
+				res.status(404).json({
+					message: "ไม่พบสินค้าในระบบ!",
+				});
+			}
+		} catch (error) {
+			if (error.errors) {
+				error.errors = error.errors.map((err) => [err.message, err.path, err.instance[err.path]]);
+				error.errors = error.errors.map((err) => {
+					return {
+						message: err[0],
+						path: err[1],
+						value: err[2],
+					};
+				});
+				res.status(500).json(error.errors);
+			} else {
+				res.status(500).json(error);
+			}
+		}
+		next();
 	},
 
-	addCategory: async (req, res, next) => {
-	},
+	showCategory: async (req, res, next) => {},
 
-	deleteCategory: async (req, res, next) => {
-	},
+	addCategory: async (req, res, next) => {},
 
-	updateCategory: async (req, res, next) => {
-	},
+	deleteCategory: async (req, res, next) => {},
 
-	showCustomer: async (req, res, next) => {
-	},
+	updateCategory: async (req, res, next) => {},
 
-	deleteCustomer: async (req, res, next) => {
-	},
+	showCustomer: async (req, res, next) => {},
 
-	updateCustomer: async (req, res, next) => {
+	deleteCustomer: async (req, res, next) => {},
 
-	},
+	updateCustomer: async (req, res, next) => {},
 };
--- backend/controller/all.controller.js
+++ backend/controller/all.controller.js
@@ -2,10 +2,63 @@ const {items} = require("../model");
 
 module.exports = {
 	show: async (req, res, next) => {
+		try {
+			const item = await items.findAll();
+			if (item) {
+				res.status(200).json({
+					message: "Get data of product success",
+					item,
+				});
+			} else {
+				res.status(404).json({
+					message: "Can not get data of product",
+				});
+			}
+		} catch (error) {
+			next(error);
+		}
+		next();
 	},
 
 	showDetail: async (req, res, next) => {
+		try {
+			const item = await items.findByPk(req.params.id);
+			if (item) {
+				res.status(200).json({
+					message: "Get product success",
+					item,
+				});
+			} else {
+				res.status(404).json({
+					message: "Product not found",
+				});
+			}
+			next();
+		} catch (error) {
+			next(error);
+		}
 	},
 
-	search: async (req, res, next) => {}
-}
+	search: async (req, res, next) => {
+		try {
+			const {keyword} = req.params;
+			let item = await items.findAll();
+			item = item.filter((goods) => {
+				return goods.name.toLowerCase().includes(keyword.toLowerCase());
+			});
+			if (item) {
+				res.status(200).json({
+					message: " Search product success",
+					item,
+				});
+			} else {
+				res.status(404).json({
+					message: "Product not found",
+				});
+			}
+		} catch (error) {
+			next(error);
+		}
+		next();
+	},
+};
